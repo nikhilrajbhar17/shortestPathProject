e7f50fce7ee249d3777068a1b0737d7c3a215904
nikhilrajbhar17 <nikhilrajbhar135@gmail.com>
2024-07-06 18:51:28 +0530

    this one's little better

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./style.css";
+import "./styles.css";
+import "./st.css";
+import "./s.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
